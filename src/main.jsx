b5e4cb68e300a693882f07dbd2d5c85a216f148c
Praveen8161 <rbalu8161@gmail.com>
2023-12-31 08:10:15 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { configureStore } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux'
-import CartReducers from './Reducers/CartReducers.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+// cart reducer refers to cartSlice.reducer
+import CartReducers from "./Reducers/CartReducers.js";
 
 const store = configureStore({
   reducer: {
+    // Same as the slice name
     cartOperation: CartReducers,
-  }
+  },
 });
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store} >
-      <App/>
+    <Provider store={store}>
+      <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
